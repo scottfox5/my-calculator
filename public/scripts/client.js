fa63a84bd0cb12cbd8c5f2d13215a7e7bd39a7c3
Scott Fox <scottfox5@gmail.com>
2017-01-22 00:36:34 -0600

    refactored code and updated style

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,74 +1,106 @@
 $(function(){
   // different buttons to handle each operator
-  $('#add').click(add);
-  $('#sub').click(subtract);
-  $('#mul').click(multiply);
-  $('#div').click(divide);
+  // $('#add').click(add);
+  // $('#sub').click(subtract);
+  // $('#mul').click(multiply);
+  // $('#div').click(divide);
+  $('.operator').click(calculate);
   $('#clear').click(clear);
 
 });
 
-
-function add (){
+function calculate (){
+  var buttonPressed = $(this).html();
 
   event.preventDefault();// stop the browser from trying to navigate away from our page
-  var xyCalc = $('form').serialize();// get the information out of the form
-  console.log(xyCalc);
-  // xyCalc.operator
+  var formData = $('form').serializeArray();// get the information out of the form
+  var xyCalc = {}
+  xyCalc.x = formData[0].value;
+  xyCalc.y = formData[1].value;
+
+  if (buttonPressed == '+') {
+    xyCalc.operator = 'add';
+  } else if (buttonPressed == '-') {
+    xyCalc.operator = 'sub';
+  } else if (buttonPressed == '*') {
+    xyCalc.operator = 'mul';
+  } else if (buttonPressed == '/') {
+    xyCalc.operator = 'div';
+  } else {
+    clear();
+  }
 
   $.ajax({
-    url: '/sum',
+    url: '/calc',
     type: 'POST',
-    data:  xyCalc ,
+    data:  xyCalc,
     success: appendDom,
   })
 
 }
 
 
-function subtract (){
 
-  event.preventDefault();// stop the browser from trying to navigate away from our page
-  var xyCalc = $('form').serialize();// get the information out of the form
-  console.log(xyCalc);
-
-  $.ajax({
-    url: '/sub',
-    type: 'POST',
-    data: xyCalc,
-    success: appendDom,
-  })
-}
-
-function multiply (){
-
-  event.preventDefault();// stop the browser from trying to navigate away from our page
-  var xyCalc = $('form').serialize();// get the information out of the form
-  console.log(xyCalc);
-
-  $.ajax({
-    url: '/mul',
-    type: 'POST',
-    data: xyCalc,
-    success: appendDom,
-  })
-
-}
-
-function divide (){
-
-  event.preventDefault();// stop the browser from trying to navigate away from our page
-  var xyCalc = $('form').serialize();// get the information out of the form
-  console.log(xyCalc);
-
-  $.ajax({
-    url: '/div',
-    type: 'POST',
-    data: xyCalc,
-    success: appendDom
-  })
-
-}
+//// four different functions to handle each operator click
+// function add (){
+//
+//   event.preventDefault();// stop the browser from trying to navigate away from our page
+//   var formData = $('form').serialize();// get the information out of the form
+//   var xyCalc = {}
+//
+//   $.ajax({
+//     url: '/sum',
+//     type: 'POST',
+//     data:  xyCalc,
+//     success: appendDom,
+//   })
+//
+// }
+//
+// function subtract (){
+//
+//   event.preventDefault();// stop the browser from trying to navigate away from our page
+//   var xyCalc = $('form').serialize();// get the information out of the form
+//   console.log(xyCalc);
+//
+//   $.ajax({
+//     url: '/sub',
+//     type: 'POST',
+//     data: xyCalc,
+//     success: appendDom,
+//   })
+// }
+//
+// function multiply (){
+//
+//   event.preventDefault();// stop the browser from trying to navigate away from our page
+//   var xyCalc = $('form').serialize();// get the information out of the form
+//   console.log(xyCalc);
+//
+//   $.ajax({
+//     url: '/mul',
+//     type: 'POST',
+//     data: xyCalc,
+//     success: appendDom,
+//   })
+//
+// }
+//
+// function divide (){
+//
+//   event.preventDefault();// stop the browser from trying to navigate away from our page
+//   var xyCalc = $('form').serialize();// get the information out of the form
+//   console.log(xyCalc);
+//
+//
+//   $.ajax({
+//     url: '/div',
+//     type: 'POST',
+//     data: xyCalc,
+//     success: appendDom
+//   })
+//
+// }
 
 function appendDom(data){
   console.log(data)
--- server.js
+++ server.js
@@ -7,33 +7,56 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  console.log(app.get);
+  // console.log(app.get);
   res.sendFile(path.join(__dirname, '/public/views/index.html'));
 });
 
-app.post( '/sum', function( req, res ) {
-  console.log(req.body);
-  var num = Number(req.body.x) + Number(req.body.y);
+app.post( '/calc', function( req, res ) {
+  if (req.body.operator == 'add'){
+    var num = Number(req.body.x) + Number(req.body.y);
+    res.send({num});
+  } else if (req.body.operator == 'sub') {
+    var num = Number(req.body.x) - Number(req.body.y);
+    res.send({num});
+  } else if (req.body.operator == 'mul') {
+    var num = Number(req.body.x) * Number(req.body.y);
+    res.send({num});
+  } else if (req.body.operator == 'div') {
+    var num = Number(req.body.x) / Number(req.body.y);
+    res.send({num});
+  } else {
+    res.send(400);
+  }
+
   console.log(num);
-  res.send({num});
+
 });
 
-app.post( '/sub', function( req, res ) {
- 	var num = Number(req.body.x) - Number(req.body.y);
- 	console.log(num);
-  res.send({num})
-});
 
- app.post( '/mul', function( req, res ) {
- 	var num = Number(req.body.x) * Number(req.body.y);
-  console.log(num);
-  res.send({num});
-});
-
-app.post( '/div', function( req, res ) {
- 	var num = Number(req.body.x) / Number(req.body.y);
-  console.log(num);
-  res.send({num});
- });
+//// four different POST requests to handle each type of operation
+// app.post( '/sum', function( req, res ) {
+//   console.log(req.body);
+//   var num = Number(req.body.x) + Number(req.body.y);
+//   console.log(num);
+//   res.send({num});
+// });
+//
+// app.post( '/sub', function( req, res ) {
+//  	var num = Number(req.body.x) - Number(req.body.y);
+//  // 	console.log(num);
+//   res.send({num})
+// });
+//
+//  app.post( '/mul', function( req, res ) {
+//  	var num = Number(req.body.x) * Number(req.body.y);
+//   // console.log(num);
+//   res.send({num});
+// });
+//
+// app.post( '/div', function( req, res ) {
+//  	var num = Number(req.body.x) / Number(req.body.y);
+//   // console.log(num);
+//   res.send({num});
+//  });
 
 app.listen(3000);
